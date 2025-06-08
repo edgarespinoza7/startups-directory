525ef9be1a36b0b594e0497bb46c61fb6bb2209a
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-06-09 01:38:37 +0200

    adds hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl">Hello Edgar</h1>
+  <>
+  <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+    <h1 className="heading">Pitch your Startup <br /> Connect with Entrepreneurs</h1>
+    <p className="sub-heading !max-w-3xl text-center">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+  </section>
+  </>
   );
 }
