116862ba3dde4dc42e9dc7ef92ce05f40e7f89b5
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-06-09 12:30:21 +0200

    adds search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query ?? "";
+
   return (
-  <>
-  <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
-    <h1 className="heading">Pitch your Startup <br /> Connect with Entrepreneurs</h1>
-    <p className="sub-heading !max-w-3xl text-center">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
-  </section>
-  </>
+    <>
+      <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="heading">
+          Pitch your Startup <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl text-center">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
   );
 }
