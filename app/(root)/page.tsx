43b9badad0c8139d6b7358237e43e3974ec4faa8
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-06-11 20:43:51 +0200

    adds startup groq query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_QUERY } from "@/lib/queries";
 import SearchForm from "../../components/SearchForm";
 import StartupCard from "../../components/StartupCard";
+
+interface StartupCardType {
+  _id: string;
+  title: string;
+  description: string;
+  category: string;
+  image: {
+    asset: {
+      url: string;
+    }; 
+  };
+}
 
 export default async function Home({
   searchParams,
@@ -8,50 +22,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query ?? "";
 
-  const posts = [
-    {
-      _id: 1,
-      _createdAt: new Date(),
-      views: 90,
-      author: {
-        _id: 1,
-        name: "John Doe", // Example author name
-      },
-      description: "Description for Startup A",
-      image:
-        "https://images.unsplash.com/photo-1599272771314-f3ec16bda3f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _id: 2,
-      _createdAt: new Date(),
-      views: 50,
-      author: {
-        _id: 1,
-        name: "John Doe", // Example author name
-      },
-      description: "Description for Startup B",
-      image:
-        "https://images.unsplash.com/photo-1726137570000-70ae29f0ba01?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Payments",
-      title: "ePayments",
-    },
-    {
-      _id: 3,
-      _createdAt: new Date(),
-      views: 35,
-      author: {
-        _id: 1,
-        name: "John Doe", // Example author name
-      },
-      description: "Description for Startup C",
-      image:
-        "https://images.unsplash.com/photo-1642969164999-979483e21601?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "3D Printing",
-      title: "3dMasters",
-    },
-  ];
+  const posts = await client.fetch(STARTUP_QUERY);
 
   return (
     <>
