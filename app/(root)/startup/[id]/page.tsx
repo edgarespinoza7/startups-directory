7494bee38f74f1daafc2bda5314fb74545665f9d
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-06-17 13:38:33 +0200

    adds View componenent

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
+import markdownit from "markdown-it";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
+
+const md = markdownit();
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  const parsedContent = md.render(post?.pitch || "");
 
   if (!post) {
     return notFound();
@@ -45,13 +52,30 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               />
               <div>
                 <p className="text-20-medium">{post.author?.name}</p>
-                <p className="font-medium text-[16px] text-black-300">@{post.author?.username}</p>
+                <p className="font-medium text-[16px] text-black-300">
+                  @{post.author?.username}
+                </p>
               </div>
             </Link>
 
-            
+            <p className="category-tag">{post.category}</p>
           </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+          {parsedContent ? (
+            <article
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+              className="prose max-w-4xl font-work-sans break-all"
+            ></article>
+          ) : (
+            <p className="no-result">No pitch details available</p>
+          )}
         </div>
+        <hr className="divider" />
+        {/* TODO: Editor selected startups */}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
       </section>
     </>
   );
